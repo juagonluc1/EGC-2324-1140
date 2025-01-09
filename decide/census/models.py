813afc37eec17bdad4fdb8aead5c46ec65b56231
Juan Antonio Gonzalez Lucena <juagonluc1@alum.us.es>
2025-01-09 19:45:47 +0100

    codigo

--- decide/census/models.py
+++ decide/census/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Census(models.Model):
     voting_id = models.PositiveIntegerField()
     voter_id = models.PositiveIntegerField()
-    texto1="Este es el texto para el fallo 1"
+    texto1="Este es el texto para el fallo 11"
     
     class Meta:
         unique_together = (('voting_id', 'voter_id',texto1),)
